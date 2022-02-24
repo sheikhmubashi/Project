d1649c76feb8083cf918cd6051542c8854c466ba
Sheikh Mubashir <smubashir655@gmail.com>
2022-02-25 01:36:57 +0500

    asds
    sad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import SignUp from './screens/signUp';
+import SignUp from './screens/signup/signUp';
 
 function App() {
   return (
